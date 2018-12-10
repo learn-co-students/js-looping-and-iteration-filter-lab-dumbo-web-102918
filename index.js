600b736438a760f6481fa3897578fdfe0d1c5e0d
jessanderson222 <jessanderson222@gmail.com>
2018-12-10 12:16:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file
+function findMatching(drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(function(driver) {
+    const lettersLength = letters.length;
+    const startsWith = driver.slice(0, lettersLength);
+    return startsWith === letters
+  });
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function(driver) {
+    return driver.name === string 
+  });
+}

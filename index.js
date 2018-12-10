46c7e6b20ebafb04c22fff4fc6bc8846bd7462c6
cfernando415 <ca.fernando415@gmail.com>
2018-12-10 18:58:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file
+function findMatching(list, name) {
+  return list.filter(function(driverName){
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+};
+function fuzzyMatch(list, str) {
+  return list.filter(function(driverName) {
+    return driverName.startsWith(str)
+  });
+};
+function matchName(list, str) {
+  return list.filter(function(driver) {
+    return driver.name === str;
+  });
+};
